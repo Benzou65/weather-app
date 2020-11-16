37c29c9a4855568782b40051a155045fc6328ec2
Lionel Bonzoumet <lionel.bonzoumet@gmail.com>
2020-11-16 23:49:58 +0100

    feat(Meteo): add coming days navigation

--- src/Components/Meteo/ComingDays.js
+++ src/Components/Meteo/ComingDays.js
@@ -2,17 +2,17 @@ import React from "react";
 import "./ComingDays.css";
 
 function ComingDays(props) {
-	const { day } = props;
+	const { day, onClick } = props;
 
 	return (
-		<div className="ComingDays">
+		<button className="ComingDays" type="button" onClick={onClick}>
 			<p>
 				{day.day_long} - {day.date}
 			</p>
 			<span>Min. {day.tmin}°C</span>
 			<img src={day.icon} alt={day.condition} title={day.condition} />
 			<span>Max. {day.tmax}°C</span>
-		</div>
+		</button>
 	);
 }
 
